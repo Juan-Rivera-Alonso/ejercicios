23ee29987c17108e81d7294ed4c5d51c78c4145a
Juan Rivera <themorgor@gmail.com>
2020-06-08 01:14:54 -0600

    trabajo js

---
+++ first.js
@@ -0,0 +1,41 @@
+
+//first
+/* 
+for (let hash = "#" ;
+hash.length <=7 ; hash+="#") {
+    console.log(hash);
+    }
+ */ 
+
+
+
+//second
+/* 
+
+ for ( let juan =1; juan <=100; juan++) {
+     let carlos= "";
+     if (juan % 3 == 0) carlos +="fizz";
+     if (juan % 5 ==0) carlos +="buzz";
+     console.log( carlos || juan);
+ }
+*/
+
+
+
+//third
+/*
+ let size= 20;
+ let dot = "";
+ 
+for (let columnas=0; columnas < size; columnas++){
+    for(let filas= 0; filas < size; filas++){
+        if (( filas  + columnas) % 2==0){
+            dot +=" ";
+        } else{
+            dot +="#";
+        }
+    }
+    dot += "\n";
+}
+console.log(dot);
+*/
